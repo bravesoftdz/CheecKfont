bf1c88855429b8553a2209271e47df502bf9a70a
walwalwalides <walwalwalides@gmail.com>
2019-01-24 13:20:59 +0100

    Add files via upload

---
+++ CheeckFont.dpr
@@ -0,0 +1,15 @@
+program CheeckFont;
+
+uses
+  Vcl.Forms,
+  Main in 'View\Main.pas' {FrmMain},
+  ToolLib in 'Unit\ToolLib.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFrmMain, FrmMain);
+  Application.Run;
+end.
